8d32fde380f6aecc2d4f35abfeb469aeaa1ce62c
Himanshu <himanshudhiman9313@gmail.com>
2020-11-26 12:56:24 +0530

    initial commit

---
+++ src/Components/Problem/Problem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu';
+
+
+function Problem() {
+    return (
+        <div>
+            <Menu/>
+        </div>
+    )
+}
+
+export default Problem;
